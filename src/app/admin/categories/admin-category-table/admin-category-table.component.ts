098ba82083efcaac8fe8585765b19f41cf0dd25f
alexandraalvarezquintero75-source <alexandraalvarezquintero75@gmail.com>
2025-12-30 14:43:59 -0500

    Organizacion de modulos

---
+++ src/app/admin/categories/admin-category-table/admin-category-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category-table',
+  imports: [],
+  templateUrl: './admin-category-table.component.html',
+  styleUrl: './admin-category-table.component.scss'
+})
+export class AdminCategoryTableComponent {
+
+}
---
+++ src/app/ladingPage/lading-page/lading-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lading-page',
+  imports: [],
+  templateUrl: './lading-page.component.html',
+  styleUrl: './lading-page.component.scss'
+})
+export class LadingPageComponent {
+
+}
